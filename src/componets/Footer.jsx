428589bce1b28decfbf2b08390e7b46883d257ab
zangbin <zangb@GIANG>
2025-08-20 11:49:11 +0700

    Update UI Header and Footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.component.css";
+import logo from "../assets/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="logo" />
+        </Link>
+        <p> BookWeb © {new Date().getFullYear()} - All Rights Reserved</p>
+        <div className="footer-links">
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            Facebook
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noreferrer">
+            Twitter
+          </a>
+          <a href="/about">About</a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
