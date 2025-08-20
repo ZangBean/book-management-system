86f51172bf6df41d297a1e322279709cb015449b
Anhthuan25022003 <thuanit25223@gmai.com>
2025-08-20 09:44:00 +0700

    config axios

---
+++ src/services/bookService.jsx
@@ -0,0 +1,8 @@
+// src/api/bookService.js
+import api from './config'
+
+export const getBooks = () => api.get('/books')
+export const getBookById = (id) => api.get(`/books/${id}`)
+export const createBook = (data) => api.post('/books', data)
+export const updateBook = (id, data) => api.put(`/books/${id}`, data)
+export const deleteBook = (id) => api.delete(`/books/${id}`)
