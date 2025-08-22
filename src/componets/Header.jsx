2b3ce24b39b54ba6eee1cfbe45bef2e2cdf4d29c
zangbin <zangb@GIANG>
2025-08-22 09:21:28 +0700

    Update UI header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.component.css";
 import logo from "../assets/logo.png";
 import ModalAddBook from "../pages/BookForm";
@@ -75,18 +75,27 @@ const Header = ({ books, setBooks, filtereds, setFiltereds }) => {
   return (
     <header className="header-content">
       <div className="header container">
-        <Link to="/" className="logo-link">
+        <NavLink to="/" className="logo-link">
           <img src={logo} alt="logo" />
-        </Link>
-        <Link to="/" className="nav-link">
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
           Home
-        </Link>
-        <Link to="/genre" className="nav-link">
+        </NavLink>
+        <NavLink
+          to="/genre"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
           Genre
-        </Link>
-        <Link to="/about" className="nav-link">
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
           About
-        </Link>
+        </NavLink>
 
         <div className="search-container">
           <input
